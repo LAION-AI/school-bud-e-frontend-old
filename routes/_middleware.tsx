2211ce9e4eb3b1c8530749da279c636249465f8e
m5x5 <michaelpeters5647@gmail.com>
2025-04-02 22:31:47 +0200

    Add logging to middleware for tracking requests

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -2,6 +2,7 @@ import type { FreshContext } from "$fresh/server.ts";
 import { trackPageView } from "../utils/umami.ts";
 
 export async function handler(req: Request, ctx: FreshContext) {
+  console.log("middleware");
     trackPageView(req);
     const resp = await ctx.next();
     return resp;
